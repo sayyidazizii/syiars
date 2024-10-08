0e7eb82341f8feffbbab57f040989a13a7edda8f
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-10-08 09:44:31 +0700

    enhance(SystemUserMigration):migrasi system user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,13 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('system_user', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->unsignedBigInteger('company_id')->default(1)->nullable();
+            $table->unsignedBigInteger('user_group_id')->nullable();
+            $table->string('full_name',255)->nullable();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('phone_number',255)->nullable();
+            $table->integer('section_id')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->smallInteger('data_state')->default(0)->nullable();
+            $table->unsignedBigInteger('branch_id')->default(1)->nullable();
+            $table->unsignedBigInteger('created_id')->nullable();
+            $table->unsignedBigInteger('updated_id')->nullable();
+            $table->uuid('uuid')->nullable();
+            $table->unsignedBigInteger('deleted_id')->nullable();
+            $table->softDeletes(); // Menambahkan kolom deleted_at
             $table->timestamps();
         });
 
