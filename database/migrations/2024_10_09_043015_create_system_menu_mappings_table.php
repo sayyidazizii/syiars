4182d16d19b49bc5c41b0c6ac5cb8551d7f88cce
Meysellin <Meysellinnnn@gmail.com>
2024-10-09 14:20:28 +0700

    enhance(SystemMenuMapping):Schema::dropIfExists('system_menu_mapping');

--- database/migrations/2024_10_09_043015_create_system_menu_mappings_table.php
+++ database/migrations/2024_10_09_043015_create_system_menu_mappings_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('system_menu_mappings');
+        Schema::dropIfExists('system_menu_mapping');
     }
 };
