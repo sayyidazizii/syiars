f1b81269c09a3097e986a4ed0342a3145901c984
evinovita13 <evi770994@gmail.com>
2024-10-18 15:59:05 +0700

    enhance(SystemUserGroupMigration):migrasi acct_deposito

---
+++ database/factories/AcctDepositoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AcctDeposito>
+ */
+class AcctDepositoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
