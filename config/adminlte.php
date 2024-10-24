f44f3c4c6c554f57796776cb1a4ee8ff1df288ba
Ikhsan <ikhsansetyo05@gmail.com>
2024-10-23 15:15:49 -1200

    enchange(AactAccount): membuat CRUD Nomor Perkiraan

--- config/adminlte.php
+++ config/adminlte.php
@@ -324,30 +324,7 @@ return [
         [
             'text' => 'Nomor Perkiraan',
             'url' => 'aact_account',
-            'icon' => 'fas fa-fw fa-bolt',
-        ],
-        [
-            'text' => 'blog',
-            'url' => 'admin/blog',
-            'can' => 'manage-blog',
-        ],
-        [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url' => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url' => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'icon' => 'fas fa-fw fa-beer',
         ],
         [
             'text' => 'System User Group',
@@ -355,68 +332,14 @@ return [
             'icon' => 'fas fa-fw fa-lock',
         ],
         [
-            'text' => 'Kode simpanan berjangka',
+            'text' => 'Kode Simpanan Berjangka',
             'url' => 'AcctDeposito',
-            'icon' => 'fas fa-fw fa-lock',
+            'icon' => 'fas fa-fw fa-gavel',
         ],
         [
             'text' => 'Kode Simpanan',
             'url' => 'AcctSavings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
-        ],
-        ['header' => 'menu'],
-        [
-            'text' => 'important',
-            'icon_color' => 'red',
-            'url' => '#',
-        ],
-        [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
-            'url' => '#',
-        ],
-        [
-            'text' => 'information',
-            'icon_color' => 'cyan',
-            'url' => '#',
+            'icon' => 'fas fa-fw fa-th',
         ],
     ],
 
