503096e160f4968f8a2377b6480f145d81d23625
Meysellin <Meysellinnnn@gmail.com>
2024-10-28 15:50:47 +0700

    enhance(CoreProvince):Schema::create('core_province', function (Blueprint $table) {

---
+++ database/migrations/2024_10_28_084350_create_core_province_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_province', function (Blueprint $table) {
+            $table->increments('province_id'); 
+            $table->char('province_code', 2); 
+            $table->string('province_name', 255); 
+            $table->string('province_no', 20)->default(''); 
+            $table->smallInteger('data_state')->default(0)->nullable();
+            $table->unsignedBigInteger('branch_id')->default(1)->nullable();
+            $table->unsignedBigInteger('created_id')->nullable();
+            $table->unsignedBigInteger('updated_id')->nullable();
+            $table->uuid('uuid')->nullable();
+            $table->unsignedBigInteger('deleted_id')->nullable();
+            $table->softDeletes(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('core_province');
+    }
+};
