ba69e3644203fb06a3fdbe7a152804d19e3c2dbf
Ikhsan <ikhsansetyo05@gmail.com>
2024-10-29 10:36:42 -1200

    enhange(CoreOffice): Membuat Model Tabel

--- app/Models/CoreOffice.php
+++ app/Models/CoreOffice.php
@@ -3,10 +3,47 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class CoreOffice extends Model
 {
     /** @use HasFactory<\Database\Factories\CoreOfficeFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    use Notifiable;
+
+    protected $table = 'core_office'; // Specify the table name
+
+    protected $primaryKey = 'office_id'; // Primary key column
+
+    protected $fillable = [
+        'office_id',
+        'branch_id',
+        'user_id',
+        'office_code',
+        'office_name',
+        'data_state',
+    ];
+
+    protected $dates = [
+        'created_on',
+        'deleted_at',
+    ];
+
+    public $timestamps = true;
+
+    protected static function booted() {
+        $userid = auth()->id(); // Menggunakan auth() dengan tanda kurung
+
+        static::creating(function ($model) use ($userid) {
+            $model->created_id = $userid;
+        });
+        static::updating(function ($model) use ($userid) { // Mengubah 'updated' menjadi 'updating'
+            $model->updated_id = $userid;
+        });
+        static::deleting(function ($model) use ($userid) {
+            $model->deleted_id = $userid;
+        });
+    }
 }
