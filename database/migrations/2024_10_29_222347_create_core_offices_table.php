3d2909ad2b1cad67ffd3badf7645ad0e513c5a30
Ikhsan <ikhsansetyo05@gmail.com>
2024-10-29 10:32:34 -1200

    enhange(CoreOffice): Membuat Migrasi tabel

---
+++ database/migrations/2024_10_29_222347_create_core_offices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_office', function (Blueprint $table) {
+            $table->increments('office_id');
+            $table->unsignedBigInteger('branch_id')->default(0);
+            $table->unsignedBigInteger('user_id')->default(0);
+            $table->string('office_code', 20)->nullable();
+            $table->string('office_name', 50)->nullable();
+            $table->smallInteger('data_state')->default(0)->nullable();
+            $table->unsignedBigInteger('created_id')->nullable();
+            $table->unsignedBigInteger('updated_id')->nullable();
+            $table->uuid('uuid')->nullable();
+            $table->unsignedBigInteger('deleted_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('core_office');
+    }
+};
