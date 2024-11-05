58caeb02df84afc45a41b9c435d6e5e863664a2d
Meysellin <Meysellinnnn@gmail.com>
2024-11-05 13:16:16 +0700

    enhance(CoreMember):

--- resources/views/content/CoreMember/add.blade.php
+++ resources/views/content/CoreMember/add.blade.php
@@ -21,98 +21,98 @@
                     <!-- Left Column -->
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="nama">Nama Anggota *</label>
-                            <input type="text" name="nama" id="nama" class="form-control" required>
+                            <label for="member_name">Nama Anggota *</label>
+                            <input type="text" name="member_name" id="member_name" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="jenis_kelamin">Jenis Kelamin *</label>
-                            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control" required>
+                            <label for="member_gender">Jenis Kelamin *</label>
+                            <select name="member_gender" id="member_gender" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
                                 <option value="Laki-Laki">Laki-Laki</option>
                                 <option value="Perempuan">Perempuan</option>
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="provinsi">Provinsi *</label>
-                            <select name="provinsi" id="provinsi" class="form-control" required>
+                            <label for="province_id">Provinsi *</label>
+                            <select name="province_id" id="province_id" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
                                 <!-- Options for provinsi go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="kabupaten">Kabupaten *</label>
-                            <select name="kabupaten" id="kabupaten" class="form-control" required>
+                            <label for="city_id">Kabupaten *</label>
+                            <select name="city_id" id="city_id" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
-                                <!-- Options for kabupaten go here -->
+                                <!-- Options for city_id go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="kecamatan">Kecamatan *</label>
-                            <select name="kecamatan" id="kecamatan" class="form-control" required>
+                            <label for="kecamatan_id">Kecamatan *</label>
+                            <select name="kecamatan_id" id="kecamatan_id" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
                                 <!-- Options for kecamatan go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="kelurahan">Kelurahan *</label>
-                            <select name="kelurahan" id="kelurahan" class="form-control" required>
+                            <label for="kelurahan_id">Kelurahan *</label>
+                            <select name="kelurahan_id" id="kelurahan_id" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
                                 <!-- Options for kelurahan go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="dusun">Dusun *</label>
-                            <input type="text" name="dusun" id="dusun" class="form-control" required>
+                            <label for="dusun_id">Dusun *</label>
+                            <input type="text" name="dusun_id" id="dusun_id" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="tempat_lahir">Tempat Lahir *</label>
-                            <input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control" required>
+                            <label for="member_place_of_birth">Tempat Lahir *</label>
+                            <input type="text" name="member_place_of_birth" id="member_place_of_birth" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="tanggal_lahir">Tanggal Lahir *</label>
-                            <input type="date" name="tanggal_lahir" id="tanggal_lahir" class="form-control" required>
+                            <label for="member_date_of_birth">Tanggal Lahir *</label>
+                            <input type="date" name="member_date_of_birth" id="member_date_of_birth" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="alamat">Alamat *</label>
-                            <textarea name="alamat" id="alamat" class="form-control" required></textarea>
+                            <label for="member_address">Alamat *</label>
+                            <textarea name="member_address" id="member_address" class="form-control" required></textarea>
                         </div>
                     </div>
 
                     <!-- Right Column -->
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="kode_pos">Kode Pos</label>
-                            <input type="text" name="kode_pos" id="kode_pos" class="form-control">
+                            <label for="member_postal_code">Kode Pos</label>
+                            <input type="text" name="member_postal_code" id="member_postal_code" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="no_telp">Nomor Telp *</label>
-                            <input type="text" name="no_telp" id="no_telp" class="form-control" required>
+                            <label for="member_phone">Nomor Telp *</label>
+                            <input type="text" name="member_phone" id="member_phone" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="sifat_anggota">Sifat Anggota *</label>
-                            <select name="sifat_anggota" id="sifat_anggota" class="form-control" required>
+                            <label for="member_character">Sifat Anggota *</label>
+                            <select name="member_character" id="member_character" class="form-control" required>
                                 <option value="">--- Pilih Salah Satu ---</option>
                                 <!-- Options for sifat anggota go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="pekerjaan">Pekerjaan</label>
-                            <input type="text" name="pekerjaan" id="pekerjaan" class="form-control">
+                            <label for="member_job">Pekerjaan</label>
+                            <input type="text" name="member_job" id="member_job" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="identitas">Identitas</label>
-                            <select name="identitas" id="identitas" class="form-control">
+                            <label for="member_identity">Identitas</label>
+                            <select name="member_identity" id="member_identity" class="form-control">
                                 <option value="">--- Pilih Salah Satu ---</option>
-                                <!-- Options for identitas go here -->
+                                <!-- Options for member_identity go here -->
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="no_identitas">Nomor Identitas</label>
-                            <input type="text" name="no_identitas" id="no_identitas" class="form-control">
+                            <label for="member_identity_no">Nomor Identitas</label>
+                            <input type="text" name="member_identity_no" id="member_identity_no" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="nama_ibu_kandung">Nama Ibu Kandung</label>
-                            <input type="text" name="nama_ibu_kandung" id="nama_ibu_kandung" class="form-control">
+                            <label for="member_mother">Nama Ibu Kandung</label>
+                            <input type="text" name="member_mother" id="member_mother" class="form-control">
                         </div>
                     </div>
                 </div>
