77cbee793a59af64a653e560d97b1637617be343
Ikhsan <ikhsansetyo05@gmail.com>
2024-11-13 13:10:42 +0700

    enhange(CoreBranch): Menambahkan Try Catch Update

--- app/Http/Controllers/CoreBranchController.php
+++ app/Http/Controllers/CoreBranchController.php
@@ -64,17 +64,25 @@ class CoreBranchController extends Controller
             'branch_phone1' => 'required|string|max:255',
             'branch_phone2' => 'required|string|max:255',
         ]);
-        $core_branch = CoreBranch::findOrFail($id);
-        $core_branch->branch_code = $request->input('branch_code');
-        $core_branch->branch_name = $request->input('branch_name');
-        $core_branch->branch_address = $request->input('branch_address');
-        $core_branch->branch_city = $request->input('branch_city');
-        $core_branch->branch_contact_person = $request->input('branch_contact_person');
-        $core_branch->branch_email = $request->input('branch_email');
-        $core_branch->branch_phone1 = $request->input('branch_phone1');
-        $core_branch->branch_phone2 = $request->input('branch_phone2');
-        $core_branch->save();
-        return redirect()->route('core_branch.index')->warning('Data Kode Cabang berhasil diperbarui!');
+        try {
+            DB::beginTransaction();
+            $core_branch = CoreBranch::findOrFail($id);
+            $core_branch->branch_code = $request->input('branch_code');
+            $core_branch->branch_name = $request->input('branch_name');
+            $core_branch->branch_address = $request->input('branch_address');
+            $core_branch->branch_city = $request->input('branch_city');
+            $core_branch->branch_contact_person = $request->input('branch_contact_person');
+            $core_branch->branch_email = $request->input('branch_email');
+            $core_branch->branch_phone1 = $request->input('branch_phone1');
+            $core_branch->branch_phone2 = $request->input('branch_phone2');
+            $core_branch->save();
+            DB::commit();
+            return redirect()->route('core_branch.index')->warning('Data Kode Cabang berhasil diperbarui!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            report($e);
+            return redirect()->route('core_branch.index')->danger('Data Kode Cabang gagal diperbarui!');
+        }
     }
     public function delete($id)
     {
